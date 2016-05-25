4d30160dd4ad5fdb7173f065caf6603f6f9829fd
Shad Acker <autoshad@gmail.com>
2016-05-25 15:35:11 -0400

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#echo -e "\x1B[01;96m  foo   \x1B[0m"
-
-echo -e "\x1B[01;96m Enter your IP or Site's URL (e.g. www.myblog.com):   \x1B[0m" 
-read site
-sed  -i -e "s/XXX/${site}/g" ghost.conf
-sed  -i -e "s/XXX/${site}/g" config.js
-
-echo using this string $site
