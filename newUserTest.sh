c31e0d085b5274860bfc0d66e5c82e2760bbff03
Shad Acker <autoshad@gmail.com>
2016-05-26 15:18:47 -0400

    new test file for users creation

---
+++ newUserTest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo -e "\x1B[01;96m Enter a Username to run your blog as:   \x1B[0m" 
+read ghostname
+
+echo -e "\x1B[01;96m Enter a Password for that account   \x1B[0m" 
+read ghostpass
+
+
+sudo useradd $ghostname
+sudo passwd $ghostpass
